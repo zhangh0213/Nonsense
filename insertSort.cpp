9755c9e2eb6cbee601bc5cacc83b443bbc87d624
zhangh <45964780+Boxxxx-zh@users.noreply.github.com>
2021-07-17 19:59:06 +0800

    Create insertSort.cpp

---
+++ insertSort.cpp
@@ -0,0 +1,11 @@
+void insertSort(std::vector<int>& nums) {
+    for (int i = 1; i < nums.size(); ++i) {
+        int key = nums[i];
+        int j = i - 1;
+        while (j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j];
+            --j;
+        }
+        nums[j + 1] = key;
+    }
+}
